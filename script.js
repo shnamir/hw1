f988b8a86442805ceeff978037ac21471d7a87ad
amir <shiodiyev.amir123@example.com>
2023-03-28 23:30:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let data = prompt('data')
+
+if(data >= 1 && data < 10){
+  alert('first decade')
+}else if (data >=10 && data < 20){
+  alert('second dekade')
+}else if (data >=20 && data < 30){
+  alert('third decade')}
